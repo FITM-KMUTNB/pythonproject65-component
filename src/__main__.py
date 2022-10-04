a9365c1642ab56285e9e7eaa3fc335a2577de4e7
Yongyuth Chuankhuntod <eine.schlacht@gmail.com>
2022-10-05 01:29:26 +0700

    main: added mixer for music.

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,30 @@
+import os
 import pygame
+from pygame.locals import *
+from pygame import mixer
 from sys import exit
 
+WIDTH: int = 1920
+HEIGHT: int = 1080
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+background_image = pygame.image.load('./src/images/root_background.jpg')
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+
+mixer.init()
+mixer.music.load('./src/music/root_music_background.mp3')
+mixer.music.play()
+mixer.music.set_volume(0.2)
 
 while True:
+    screen.blit(background_image, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
         pygame.display.update()
-        clock.tick(60)
+
+pygame.quit()
